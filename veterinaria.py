c404aafdb029bd9548d2e56b19c9d27358febc63
Rocael14 <jc.vmayen@udeo.edu.gt>
2025-07-07 21:47:03 -0600

    Comenzamos el proyecto definiendo una funcion para el Menu

---
+++ veterinaria.py
@@ -0,0 +1,11 @@
+def Menu():
+    print("--- Bienvenido a la Clínica Veterinaria ---")
+    print("1. Registrar nuevo cliente")
+    print("2. Registrar nueva mascota")
+    print("3. Agendar cita médica")
+    print("4. Ver historial de citas")
+    print("5. Ver clientes y mascotas")
+    print("0. Salir")
+
+Menu()
+opcion = int(input("Selecciona una opcion: "))
